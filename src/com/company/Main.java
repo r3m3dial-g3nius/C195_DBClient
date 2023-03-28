e6f220ecbdec562344b55580426c154bf7d10c80
r3m3dial-g3nius <steven_b913@hotmail.com>
2023-03-28 14:44:08 -0600

    gitignore added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("added gitignore");
         JDBC.openConnection();
+
         JDBC.closeConnection();
     }
 }
