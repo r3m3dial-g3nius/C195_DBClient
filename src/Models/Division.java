d78209bac6b5d8d3ed779f4c7442571b36b2d2db
r3m3dial-g3nius <steven_b913@hotmail.com>
2023-04-10 13:44:58 -0600

    Division class updated w javadocs DBDivisions udpated, javadoc'd

--- src/Models/Division.java
+++ src/Models/Division.java
@@ -1,5 +1,8 @@
 package Models;
 
+/**
+ * this class manages first class divisions data
+ */
 public class Division {
 
     private int divisionID;
@@ -19,26 +22,50 @@ public class Division {
         this.countryID = countryID;
     }
 
+    /**
+     * gets division ID number
+     * @return ID number of division
+     */
     public int getDivisionID() {
         return divisionID;
     }
 
+    /**
+     * sets division ID number
+     * @param divisionID ID number of division
+     */
     public void setDivisionID(int divisionID) {
         this.divisionID = divisionID;
     }
 
+    /**
+     * gets division name
+     * @return name of division
+     */
     public String getDivisionName() {
         return divisionName;
     }
 
+    /**
+     * sets division name
+     * @param divisionName name of division
+     */
     public void setDivisionName(String divisionName) {
         this.divisionName = divisionName;
     }
 
+    /**
+     * gets country ID number
+     * @return ID number of country
+     */
     public int getCountryID() {
         return countryID;
     }
 
+    /**
+     * sets country ID number
+     * @param countryID ID number of country
+     */
     public void setCountryID(int countryID) {
         this.countryID = countryID;
     }
